d2dbdc1b27c8500ad23743ad673e33d84b1c9211
Jonian Guveli <jonian@hardpixel.eu>
2019-10-05 12:44:38 +0300

    remove usage of Lang and Mainloop

--- hotel-manager@hardpixel.eu/extension.js
+++ hotel-manager@hardpixel.eu/extension.js
@@ -1,23 +1,22 @@
 const GLib            = imports.gi.GLib;
-const Lang            = imports.lang;
+const GObject         = imports.gi.GObject;
+const St              = imports.gi.St;
 const Main            = imports.ui.main;
-const Mainloop        = imports.mainloop;
 const PanelMenu       = imports.ui.panelMenu;
 const PopupMenu       = imports.ui.popupMenu;
-const St              = imports.gi.St;
 const ExtensionUtils  = imports.misc.extensionUtils;
 const HotelLauncher   = ExtensionUtils.getCurrentExtension();
 const Helpers         = HotelLauncher.imports.helpers;
 const PopupServerItem = HotelLauncher.imports.popupServerItem.PopupServerItem;
 const Util            = imports.misc.util;
 
-var HotelManager = new Lang.Class({
+var HotelManager = new GObject.Class({
   Name: 'HotelManager',
   Extends: PanelMenu.Button,
   _entries: {},
   _running: false,
 
-  _init: function() {
+  _init() {
     this._config = this._hotelConfig();
     this._uri    = this._hotelUri();
 
@@ -30,31 +29,33 @@ var HotelManager = new Lang.Class({
 
     this.add_actor(this.icon);
 
-    this.menu.connect('open-state-changed', Lang.bind(this, this._refresh));
+    this.menu.connect('open-state-changed', () => {
+      this._refresh()
+    });
 
     this._refresh();
   },
 
-  _hotelConfig: function() {
+  _hotelConfig() {
     let config = '~/.hotel/conf.json';
     let data   = { port: 2000, host: '127.0.0.1', tld: 'localhost' };
 
     return Helpers.fileGetContents(config, data, true);
   },
 
-  _hotelUri: function() {
+  _hotelUri() {
     let host = this._config.host;
     let port = this._config.port;
 
     return host + ':' + port;
   },
 
-  _getCommand: function() {
+  _getCommand() {
     let command = Helpers.fileGetLine('~/.hotelrc', 0, 'hotel');
     return Helpers.getFilePath(command);
   },
 
-  _getUrl: function (action, id) {
+  _getUrl(action, id) {
     let paths = {
       start:   '/_/servers/${id}/start',
       stop:    '/_/servers/${id}/stop',
@@ -64,35 +65,35 @@ var HotelManager = new Lang.Class({
     return this._uri + paths[action].replace('${id}', id);
   },
 
-  _checkHotel: function () {
+  _checkHotel() {
     let running = Helpers.commandGetOutput('ps -ef').match(/hotel\/lib\/daemon/);
     return running == 'hotel/lib/daemon';
   },
 
-  _toggleHotel: function (start) {
+  _toggleHotel(start) {
     let action  = start ? 'start' : 'stop';
     let command = this._getCommand();
 
     Util.spawn([command, action]);
   },
 
-  _checkServer: function (server) {
+  _checkServer(server) {
     return server.status && server.status == 'running';
   },
 
-  _toggleServer: function (id, start) {
+  _toggleServer (id, start) {
     let action = start ? 'start' : 'stop';
     let url    = this._getUrl(action, id);
 
     GLib.spawn_command_line_sync('curl --request POST ' + url);
   },
 
-  _openServerUrl: function (id) {
+  _openServerUrl (id) {
     let url = 'http://' + id + '.' + this._config.tld;
     Util.spawn(['xdg-open', url]);
   },
 
-  _getServers: function () {
+  _getServers() {
     let items = {};
 
     if (this._running) {
@@ -103,14 +104,14 @@ var HotelManager = new Lang.Class({
     return items;
   },
 
-  _addServerItems: function () {
+  _addServerItems() {
     let servers = Object.keys(this._entries);
     if (!servers.length) return;
 
     let separator = new PopupMenu.PopupSeparatorMenuItem();
     this.menu.addMenuItem(separator);
 
-    servers.forEach(Lang.bind(this, function(id, index) {
+    servers.forEach((id, index) => {
       let server     = this._entries[id];
       let active     = this._checkServer(server);
       let options    = { restartButton: true, launchButton: true };
@@ -118,42 +119,36 @@ var HotelManager = new Lang.Class({
 
       this.menu.addMenuItem(serverItem.widget);
 
-      serverItem.widget.connect('toggled', Lang.bind(this, function(button, state) {
+      serverItem.widget.connect('toggled', (button, state) => {
         this._toggleServer(id, state);
         this._setServerItemState(button, id);
-      }));
+      });
 
-      serverItem.launchButton.connect('clicked', Lang.bind(this, function(event) {
+      serverItem.launchButton.connect('clicked', (event) => {
         this._openServerUrl(id);
         this.menu.close();
-      }));
-    }));
+      });
+    });
   },
 
-  _setServerItemState: function(serverItem, server) {
+  _setServerItemState(serverItem, server) {
     serverItem.setSensitive(false);
 
-    Mainloop.timeout_add(500, Lang.bind(this, function() {
-      this._entries = this._getServers();
-      let curServer = this._entries[server];
+    this._entries = this._getServers();
+    let curServer = this._entries[server];
 
-      serverItem.setToggleState(this._checkServer(curServer));
-      serverItem.setSensitive(true);
-    }));
+    serverItem.setToggleState(this._checkServer(curServer));
+    serverItem.setSensitive(true);
   },
 
-  _setHotelItemState: function(hotelItem) {
-    hotelItem.setSensitive(false);
+  _setHotelItemState(hotelItem) {
+    this._running = this._checkHotel();
 
-    Mainloop.timeout_add(500, Lang.bind(this, function() {
-      this._running = this._checkHotel();
-
-      hotelItem.setToggleState(this._running);
-      hotelItem.setSensitive(true);
-    }));
+    hotelItem.setToggleState(this._running);
+    hotelItem.setSensitive(true);
   },
 
-  _refresh: function() {
+  _refresh() {
     this.menu.removeAll();
 
     this._running = this._checkHotel();
@@ -168,17 +163,17 @@ var HotelManager = new Lang.Class({
     let hotelItem = new PopupServerItem('Hotel', this._running, options);
     this.menu.addMenuItem(hotelItem.widget);
 
-    Mainloop.idle_add(Lang.bind(this, this._addServerItems));
+    this._addServerItems();
 
-    hotelItem.widget.connect('toggled', Lang.bind(this, function(button, state) {
+    hotelItem.widget.connect('toggled', (button, state) => {
       this._toggleHotel(state);
       this._setHotelItemState(button);
-    }));
+    });
 
-    hotelItem.launchButton.connect('clicked', Lang.bind(this, function() {
+    hotelItem.launchButton.connect('clicked', () => {
       this._openServerUrl('hotel');
       this.menu.close();
-    }));
+    });
   }
 });
 
--- hotel-manager@hardpixel.eu/popupServerItem.js
+++ hotel-manager@hardpixel.eu/popupServerItem.js
@@ -1,11 +1,11 @@
-const Lang      = imports.lang;
-const PopupMenu = imports.ui.popupMenu;
+const GObject   = imports.gi.GObject;
 const St        = imports.gi.St;
+const PopupMenu = imports.ui.popupMenu;
 
-var PopupServerItem = new Lang.Class({
+var PopupServerItem = new GObject.Class({
   Name: 'PopupServerItem',
 
-  _init: function(text, active, params) {
+  _init(text, active, params) {
     this.params = params || {};
     this.widget = new PopupMenu.PopupSwitchMenuItem(text, active);
 
@@ -13,12 +13,12 @@ var PopupServerItem = new Lang.Class({
     this._launchButton();
   },
 
-  _icon: function(icon_name) {
+  _icon(icon_name) {
     let icon = new St.Icon({ icon_name: icon_name });
     return icon;
   },
 
-  _button: function(button_name, icon_name) {
+  _button(button_name, icon_name) {
     let options = {
       x_align:         1,
       reactive:        true,
@@ -34,20 +34,20 @@ var PopupServerItem = new Lang.Class({
     return button;
   },
 
-  _restartButton: function() {
+  _restartButton() {
     if (!this.params.restartButton) return;
 
     this.restartButton = this._button('restart', 'view-refresh-symbolic');
     this.widget.add(this.restartButton, { expand: false, x_align: St.Align.END });
 
-    this.restartButton.connect('clicked', Lang.bind(this, function() {
+    this.restartButton.connect('clicked', () => {
       this.widget.setToggleState(false);
       this.widget.emit('toggled', false);
       this.widget.emit('toggled', true);
-    }));
+    });
   },
 
-  _launchButton: function() {
+  _launchButton() {
     if (!this.params.launchButton) return;
 
     this.launchButton = this._button('launch', 'network-workgroup-symbolic');
